c640dac56349ae661f68fea06883d7a320005bad
vmathieu <vm.mathieu@gmail.com>
2018-08-12 01:49:19 +0200

    fix mispelling

--- utils.go
+++ utils.go
@@ -1,14 +1,14 @@
 package logrz
 
 //padRight pad string on the right on the str if len(str)< length
-func padRight(str, pad string, lenght int) string {
-	if len(str) >= lenght {
+func padRight(str, pad string, length int) string {
+	if len(str) >= length {
 		return str
 	}
 	for {
 		str += pad
-		if len(str) > lenght {
-			return str[0:lenght]
+		if len(str) > length {
+			return str[0:length]
 		}
 	}
 }
